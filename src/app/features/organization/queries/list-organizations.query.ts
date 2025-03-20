139673d8b18741e00fd0ba905f713ab0b517128d
krystian <krystian.bondaruk@webmakers.expert>
2025-03-20 19:44:08 +0100

    feat(INV-34): fix name type

--- src/app/features/organization/queries/list-organizations.query.ts
+++ src/app/features/organization/queries/list-organizations.query.ts
@@ -1,7 +1,7 @@
 import type { IQuery } from '@/lib/cqrs/query-bus';
-import type { PaginationParamsDto } from '@/shared/pagination-utils/pagination-utils';
+import type { IPaginationParamsDto } from '@/shared/pagination-utils/pagination-utils';
 
-export class ListOrganizationsQuery implements IQuery<PaginationParamsDto> {
+export class ListOrganizationsQuery implements IQuery<IPaginationParamsDto> {
   public type = 'LIST_ORGANIZATIONS';
-  public constructor(public params: PaginationParamsDto) {}
+  public constructor(public params: IPaginationParamsDto) {}
 }
